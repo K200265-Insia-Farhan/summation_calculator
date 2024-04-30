ea769e0906df5252057826079c8d32af6d8f6552
K200265-Insia-Farhan <145255205+K200265-Insia-Farhan@users.noreply.github.com>
2024-04-30 22:36:57 +0500

    Update sumCalculatorTests.java-final

--- src/test/java/sumCalculatorTests.java
+++ src/test/java/sumCalculatorTests.java
@@ -4,21 +4,21 @@ import static org.junit.jupiter.api.Assertions.*;
 public class sumCalculatorTests {
     @Test
     public void testSumGreaterThan100() {
-        assertEquals("140", sumCalculator.calculateSum(80, 60,1,140));
+        assertEquals("140", sumCalculator.calculateSum(80, 60,1,"140"));
     }
 
     @Test
     public void testSumEquals100() {
-        assertEquals("2100", sumCalculator.calculateSum(30, 70,2,2100));
+        assertEquals("2100", sumCalculator.calculateSum(30, 70,2,"2100"));
     }
 
     @Test
     public void testSumLessThan100() {
-        assertEquals("30", sumCalculator.calculateSum(10, 40,3,30));
+        assertEquals("30", sumCalculator.calculateSum(10, 40,3,"30"));
     }
 
     @Test
     public void testSameNumbers() {
-        assertEquals("Error", sumCalculator.calculateSum(-20, -20,4,0));
+        assertEquals("Error", sumCalculator.calculateSum(-20, -20,4,"Error"));
     }
 }
