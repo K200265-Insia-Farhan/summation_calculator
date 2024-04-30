d70e37f9868bf6ad3bc74d710306275cb983a360
K200265-Insia-Farhan <145255205+K200265-Insia-Farhan@users.noreply.github.com>
2024-04-30 22:35:38 +0500

    Update sumCalculator.java-final

--- src/main/java/sumCalculator.java
+++ src/main/java/sumCalculator.java
@@ -1,11 +1,12 @@
 public class sumCalculator {
-    public static String calculateSum(int firstValue, int secondValue,int testcaseid,int expectedValue) {
+    public static String calculateSum(int firstValue, int secondValue,int testcaseid,String expectedValue) {
 
        System.out.println("--------------------------------");
        System.out.println("Test Case Id is: "+testcaseid);
        System.out.println("First Number Passed: "+firstValue);
        System.out.println("Second Number Passed is: "+secondValue);
        System.out.println("Expected Result is: " +expectedValue);
+
         if (firstValue <= 0 || secondValue <= 0) {
             System.out.println("Validated Output is: Error");
             return "Error";
@@ -15,10 +16,10 @@ public class sumCalculator {
             System.out.println("Validated Output is: "+sum);
             return String.valueOf(sum);
         } else if (sum == 100) {
-            System.out.println("Validated Output is: "+sum);
+            System.out.println("Validated Output is: "+(firstValue * secondValue));
             return String.valueOf(firstValue * secondValue);
         } else {
-            System.out.println("Validated Output is: "+sum);
+            System.out.println("Validated Output is: "+(Math.abs(firstValue - secondValue)));
             return String.valueOf(Math.abs(firstValue - secondValue));
         }
     }
